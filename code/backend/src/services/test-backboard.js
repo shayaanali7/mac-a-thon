978c41134800fa9024e13f0d3cf392ce4c94968a
Novak Vukojicic <nvukojic@uwo.ca>
2026-02-08 02:39:39 -0500

    Tuned the Analyzer Model

--- code/backend/src/services/test-backboard.js
+++ code/backend/src/services/test-backboard.js
@@ -2,47 +2,22 @@ const backboard = require("./backboard.js");
 
 (async () => {
     // console.log('Assistants:', await backboard.createAssistant());
-    // console.log('Assistants:', await backboard.deleteAssistant('80c9fca0-5c1e-40dd-a62c-f2a8d505bbc3'));
-    console.log('Assistants:', await backboard.listAssistants());
-    // console.log('Deleting all threads:', await backboard.createThreadAnalyzer('4b0c46b1-6f9b-4d53-95bc-eda6684fd35e'));
-    console.log('Threads:', await backboard.listThreads());
-    // console.log('Test:', await backboard.createThreadAnalyzer('80c9fca0-5c1e-40dd-a62c-f2a8d505bbc3'));
-    console.log('Creating message:', await backboard.sendMessageAnalyzer('2bb976c2-929c-4a1e-a92a-3e106d2c1d1a', [
+    // console.log('Assistants:', await backboard.deleteAssistant('4b0c46b1-6f9b-4d53-95bc-eda6684fd35e'));
+    // console.log('Assistants:', await backboard.listAssistants());
+    // console.log('Deleting all threads:', await backboard.createThreadAnalyzer('6743d1d7-0a1c-4f1d-b0ac-5250423d4722'));
+    // console.log('Threads:', await backboard.listThreads());
+    console.log('Creating message:', await backboard.sendMessageAnalyzer('7df9d1ba-0fb8-47bb-b2ef-a8bd8b5032bb', [
   {
     speakerId: 'speaker_0',
-    text: 'Hey there, how you doing?',
-    start: 1.299,
-    end: 2.939
+    text: 'Hey, Doc, how was walking yesterday?',
+    start: 0.68,
+    end: 2.66
   },
   {
     speakerId: 'speaker_1',
-    text: "I'm, I'm good.",
-    start: 3.079,
-    end: 5.3
-  },
-  {
-    speakerId: 'speaker_0',
-    text: "That's fire.",
-    start: 5.299,
-    end: 6.76
-  },
-  {
-    speakerId: 'speaker_0',
-    text: "What's the problem?",
-    start: 6.799,
-    end: 8.119
-  },
-  {
-    speakerId: 'speaker_1',
-    text: 'Uh, I broke my knee.',
-    start: 8.199,
-    end: 10.639
-  },
-  {
-    speakerId: 'speaker_0',
-    text: 'Good for you.',
-    start: 10.68,
-    end: 12.099
+    text: "Yeah, my diagnosis is that, uh, you have arm rip-off ellitis, and pretty much I need-- you need to take Advils for a week, and that's it.",
+    start: 3.779,
+    end: 17.199
   }
 ]));
 })();
